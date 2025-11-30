90cf04b5254545cd3b31785f0cee49aa3ddafd82
atticusjxn <atticusjxn@gmail.com>
2025-11-30 17:04:29 +1000

    Bump version to 1.1.1 for App Store submission

--- app.config.js
+++ app.config.js
@@ -11,7 +11,7 @@ export default {
     ],
     name: "FlynnAI",
     slug: "FlynnAI",
-    version: "1.1.0",
+    version: "1.1.1",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "light",
